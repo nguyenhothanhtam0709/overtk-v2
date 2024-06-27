ad031dd41a0951aa8f2d6b828a5bef29f4a5751a
tam.nguyen <tam.nguyen@gmail.com>
2024-06-27 09:13:38 +0700

    feat: update export for class decorators

--- src/core/class/decorators/index.ts
+++ src/core/class/decorators/index.ts
@@ -1,25 +1,7 @@
-import type { TypedClassDecorator } from '@overtk/core/typing/decorator/typed-class-decorator';
-import * as ClassPropertyDecorator from './class-properties';
-import { ClassDecorator, type ClassDecoratorOptions } from './class.decorator';
-
-export function Class<T extends object>(
-  options: ClassDecoratorOptions = {},
-): TypedClassDecorator<T> {
-  return ClassDecorator<T>(options);
-}
-
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace Class {
-  //
-  // Class decorator
-  //
-  export import Decorate = Class;
-
-  //
-  // Class Property decorators
-  //
-  export import StringProperty = ClassPropertyDecorator.StringPropertyDecorator;
-  export import NumberProperty = ClassPropertyDecorator.NumberPropertyDecorator;
-  export import BooleanProperty = ClassPropertyDecorator.BooleanPropertyDecorator;
-  export import EnumProperty = ClassPropertyDecorator.EnumPropertyDecorator;
-}
+export {
+  StringPropertyDecorator as StringProperty,
+  NumberPropertyDecorator as NumberProperty,
+  BooleanPropertyDecorator as BooleanProperty,
+  EnumPropertyDecorator as EnumProperty,
+} from './class-properties';
+export { ClassDecorator as Class } from './class.decorator';
